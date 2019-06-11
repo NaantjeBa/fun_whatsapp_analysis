2f77e74a3acb4bd26c878fea3fb6be4c8f5faa03
Jesse Niens <jesseniens@gmail.com>
2019-06-11 18:43:36 +0200

    Cleaned the create_master_csv module

--- src/create_master_csv.py
+++ src/create_master_csv.py
@@ -11,39 +11,28 @@ logging.basicConfig(level=logging.DEBUG,
                     )
 
 
-# def create_empty_df():
-#     master_df = pd.DataFrame(columns=['datetime', 'user', 'message'])
-#
-#     return master_df
-
-
 def change_to_masterdf_dir():
     os.chdir("C:\\Users\\jniens\\GoogleDrive\\Python_Projects\\fun_whatsapp_analysis\\data_files\\3_master_csv_fact")
 
 
 def get_master_df():
-    filename = "master_fact_good_date.csv"
-    df_master = pd.read_csv(filename, sep=';', names=['datetime', 'user', 'message'])
+    filename = "master_fact.csv"
+    df_master = pd.read_csv(filename, sep=';')
 
-    log_topic = df_master
-    # logging.debug(type(log_topic))
-    # logging.debug(len(log_topic))
-    # logging.debug(log_topic.columns)
-    # logging.debug(log_topic.datetime.min())
-    # logging.debug(log_topic.user.unique())
-    #logging.debug(log_topic.head())
+    df_master['datetime'] = pd.to_datetime(df_master['datetime'])
+
+    logging.debug(f'Read in master_fact with {len(df_master)} records')
 
     return df_master
 
 
-def get_android_temp_csvs():
+def get_temp_csvs_to_add():
     "Get list with every txt file"
     mypath = "C:\\Users\\jniens\\GoogleDrive\\Python_Projects\\fun_whatsapp_analysis\\data_files\\2_temp_csv_fact\\android"
     file_list = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-    # log_topic = file_list
-    # logging.debug(type(log_topic))
-    # logging.debug(log_topic)
+    logging.debug(f'Going to add these files: {file_list}')
+
     return file_list
 
 
@@ -53,8 +42,9 @@ def change_to_android_dir():
 
 def _read_in_temp_fact(filename):
     "read in txt file to df"
-    df = pd.read_csv(filename, sep=r';', names=['datetime', 'user', 'message'])
+    df = pd.read_csv(filename, sep=';', names=['datetime', 'user', 'message'])
 
+    logging.debug(f'Adding: {filename}...')
     # log_topic = df
     # logging.debug(len(log_topic))
     # logging.debug(log_topic.head(3))
@@ -67,56 +57,35 @@ def _append_to_masterdf(master_df, df):
 
     master_df = master_df.append(df)
 
-    # log_topic = master_df
-    # logging.debug(len(log_topic))
-    # logging.debug(log_topic.head(3))
+    logging.debug(f'Facttable is now {len(master_df)} long')
+
     return master_df
 
 
 def loop_append_to_masterdf(master_df, file_list):
     for file in file_list:
         df = _read_in_temp_fact(file)
-        df = create_datetime_type(df)
+        df = _create_datetime_type(df)
         master_df = _append_to_masterdf(master_df, df)
 
-    # log_topic = master_df
-    # logging.debug(len(log_topic))
-    #logging.debug(log_topic.head(3))
     return master_df
 
 
 def correct_names(master_df):
 
-    # log_topic = master_df
-    # logging.debug(len(log_topic.user.unique()))
-    # logging.debug(log_topic.user.unique())
-
     master_df.loc[master_df.user == 'Jesse', 'user'] = 'Jesse Niëns'
-
-    # logging.debug(len(log_topic.user.unique()))
-    # logging.debug(log_topic.user.unique())
 
     return master_df
 
 
 def filterout_fun_as_user(master_df):
-    # log_topic = master_df
-    # logging.debug(len(log_topic))
-    # logging.debug(len(log_topic.user.unique()))
-    # logging.debug(log_topic.user.unique())
 
     df_filtered_fun = master_df[master_df.user != 'Fun Fun Fun']  # Filter out Fun Fun Fun User
-
-    # log_topic = df_filtered_fun
-    # logging.debug(len(log_topic))
-    # logging.debug(len(log_topic.user.unique()))
-    # logging.debug(log_topic.user.unique())
 
     return df_filtered_fun
 
 
 def remove_duplicates(master_df):
-    logging.debug(len(master_df))
 
     df_without_duplicates = master_df.drop_duplicates(subset=[
         'datetime',
@@ -124,8 +93,8 @@ def remove_duplicates(master_df):
         'user'
     ])
 
-    # logging.debug(len(df_without_duplicates))
-    # logging.debug(f'Removed {round( (len(master_df)-len(df_without_duplicates)) / len(master_df) * 100, 2)} percent')
+    logging.debug(f'Removed {(len(master_df)-len(df_without_duplicates))} duplicate records')
+    logging.debug(f'Which is {round( (len(master_df)-len(df_without_duplicates)) / len(master_df) * 100, 2)} percent')
 
     return df_without_duplicates
 
@@ -136,14 +105,12 @@ def sort_datetime(master_df):
     return master_df
 
 
-def create_datetime_type(df):
+def _create_datetime_type(df):
 
     df['datetime'] = pd.to_datetime(df['datetime']
                                     , format='%d-%m-%y %H:%M'
                                     )
 
-    logging.debug(df.datetime.min())
-    logging.debug(df.datetime.max())
     return df
 
 
@@ -151,24 +118,21 @@ def change_to_master_dir():
     os.chdir("C:\\Users\\jniens\\GoogleDrive\\Python_Projects\\fun_whatsapp_analysis\\data_files\\3_master_csv_fact")
 
 
-def new_master_df_to_csv(master_df):
+def write_new_master_csv(master_df):
 
     filename = 'new_master_facttable.csv'
     master_df.to_csv(filename, sep=';', encoding='utf-8', index=False)
 
 
-
 change_to_masterdf_dir()
 master_df = get_master_df()
-master_df = create_datetime_type(master_df)
-# change_to_android_dir()
-# file_list_android = get_android_temp_csvs()
-# raw_master_df = loop_append_to_masterdf(master_df, file_list_android)
-# df_correct_names = correct_names(raw_master_df)
-# df_filtered_fun = filterout_fun_as_user(df_correct_names)
-# df_clean = remove_duplicates(df_filtered_fun)
-# df_datetime = create_datetime_type(df_clean)
-# df_sorted = sort_datetime(df_datetime)
-# change_to_master_dir()
-# new_master_df_to_csv(df_sorted)
+change_to_android_dir()
+file_list_android = get_temp_csvs_to_add()
+raw_master_df = loop_append_to_masterdf(master_df, file_list_android)
+df_correct_names = correct_names(raw_master_df)
+df_filtered_fun = filterout_fun_as_user(df_correct_names)
+df_clean = remove_duplicates(df_filtered_fun)
+df_sorted = sort_datetime(df_clean)
+change_to_master_dir()
+write_new_master_csv(df_sorted)
 
